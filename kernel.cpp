84453ffa46dea5a5ac43794fe06dc90dea943a35
Kross1de <awesome.hubygu@yandex.ru>
2025-04-28 15:42:18 +0600

    some types

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,9 @@
 
+#include "types.h"
+
 void printf(char*str)
 {
-	unsigned short*VideoMemory=(unsigned short*)0xb8000;
+	static uint16_t*VideoMemory=(uint16_t*)0xb8000;
 	
 	for(int i=0;str[i]!='\0';++i)
 		VideoMemory[i]=(VideoMemory[i]&0xFF00)|str[i];
@@ -9,7 +11,7 @@ void printf(char*str)
 }
 
 
-extern "C" void kernelMain(void*multiboot_structure,unsigned int magicnumber)
+extern "C" void kernelMain(const void*multiboot_structure,uint32_t /*multiboot_magic*/)
 {
 	printf("Hello, World!\n");
 	
