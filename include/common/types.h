bef959059668882bd44896b9d3705c1716dd9369
Kross1de <awesome.hubygu@yandex.ru>
2025-05-02 07:13:13 +0000

    Update types.h

--- include/common/types.h
+++ include/common/types.h
@@ -14,6 +14,8 @@ namespace myos
         typedef unsigned int           uint32_t;
         typedef long long int           int64_t;
         typedef unsigned long long int uint64_t;
+
+        typedef const char*             string;
     
     }
 }
